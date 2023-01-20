b818786e2462a85ff709a245f4a15324d600a02d
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2023-01-20 13:58:19 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Home from "./Pages/Home/Home";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
